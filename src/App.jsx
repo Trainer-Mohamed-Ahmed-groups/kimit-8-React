c792ef90baae55ea62c51b1b5395fedca0a9c386
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-11-21 20:03:06 +0200

    session-one

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Bio from './components/Bio'
+import Header from './components/Header'
+import TodoList from './components/Todolist'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className="test">esdrftgyhu  </div>
+      <div>Hello</div>
+      <Bio />
+      <TodoList />
+      <div>Hello</div>
+    </>
+  )
+}
+
+export default App
